fcb425a9990603130248ff0c6a0cea86d2b62576
Hamza Ali Rizvi <hamzaali151214@hotmail.com>
2019-12-08 19:32:44 +0530

    refactoring, destructuring and freezing

--- Objects.js
+++ Objects.js
@@ -1,53 +1,66 @@
-let person={
-    name:"Hamza"
-}
-person.age=19
+let person = {
+  name: "Hamza"
+};
+person.age = 19;
 
-console.log(person)
+console.log(person);
 //primitive values always get passed by value whereas non primitve values get passed by
 //reference all the time
-let someArray=[]
-someArray.name="hamza"
-console.log(someArray)
+let someArray = [];
+someArray.name = "hamza";
+console.log(someArray);
 
-console.log(typeof someArray)
+console.log(typeof someArray);
 //arrays are just objects with just some cool methods
-console.log(typeof someArray==='object')//will evaluate to true
-console.log(typeof person==='object')//same here
+console.log(typeof someArray === "object"); //will evaluate to true
+console.log(typeof person === "object"); //same here
 //we can use a dot operator to access properties if they are identified by a string literal
 //like:
-console.log(person.name)
+console.log(person.name);
 //but not when we have numbers
-const someOtherArray=[]
-someOtherArray.push(34)
-console.log(someOtherArray[0])
-console.log(someOtherArray.length)
+const someOtherArray = [];
+someOtherArray.push(34);
+console.log(someOtherArray[0]);
+console.log(someOtherArray.length);
 //someOtherArray[catchphrase]='Exception handlers love catchphrases'//this will give an error
-someOtherArray["catchphrase"]='Exception handlers love catchphrases'//this won't
-console.log(someOtherArray)
-console.log(someOtherArray.length)//notice that it's length stays the same
+someOtherArray["catchphrase"] = "Exception handlers love catchphrases"; //this won't
+console.log(someOtherArray);
+console.log(someOtherArray.length); //notice that it's length stays the same
 //this is because when we do stuff with someOtherArray.something or someOtherArray["something"]
 //then we are working with the array's properties and not its values
 
-
-const someObject={
-    rollno:'17103008',
-    name:'John Doe',
-    say:()=>{console.log('I am some object')}
-}
-someObject.say()
+const someObject = {
+  rollno: "17103008",
+  name: "John Doe",
+  say: () => {
+    console.log("I am some object");
+  }
+};
+someObject.say();
 
 //KEYS AND FOREACH
-const someOtherObject={
-    name:'Bruce Wayne',
-    age:19
-}
-const someOtherKeys=Object.keys(someOtherObject)
-console.log(someOtherKeys)
+const someOtherObject = {
+  name: "Bruce Wayne",
+  age: 19
+};
+const someOtherKeys = Object.keys(someOtherObject);
+console.log(someOtherKeys);
 
-someOtherKeys.forEach((element,index)=>{
-    console.log(`key: ${index} - ${element}`)
-    console.log(`value- ${someOtherObject[element]}`)
-})
+someOtherKeys.forEach((element, index) => {
+  console.log(`key: ${index} - ${element}`);
+  console.log(`value- ${someOtherObject[element]}`);
+});
 
 //Destructuring
+let [first, second] = [23, 32];
+
+//const on objects
+const anObject = {
+  name: "Pablo",
+  age: 20
+};
+anObject.name = "Mikael";
+anObject.age = 67;
+//const doesn't mean that we cannot add or modify object properties.
+//You can use Object.freeze to freeze the properties
+Object.freeze(anObject);
